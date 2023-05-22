b71de1d8ed7bc7ad1a3d28ba4b71e7c20f47669e
JmyFilm <42228457+JmyFilm@users.noreply.github.com>
2023-05-23 00:58:54 +0800

    wt1

---
+++ App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive, computed } from 'vue'
+import {  RouterView } from 'vue-router'
+import { zhCN, dateZhCN, useOsTheme, darkTheme, NConfigProvider, NMessageProvider } from 'naive-ui'
+
+const s = reactive({
+  theme: computed(() => (useOsTheme().value === 'dark' ? darkTheme : null)),
+})
+</script>
+
+<template>
+  <n-config-provider   :theme="s.theme" :locale="zhCN" :date-locale="dateZhCN">
+    <n-message-provider :max="1">
+      <RouterView class="r" />
+    </n-message-provider>
+  </n-config-provider>
+</template>
+
+<style>
+  body {
+    padding: 0;
+    margin: 0;
+  }
+  .r {
+    /* min-height: 100vh; */
+    /* border: 1px solid #000; */
+    /* background-color: #054948; */
+  }
+</style>
